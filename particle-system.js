e87e50742b44ce7b29efc8eeab5ef8f91f0832b3
Sumesh <sumesh@56:7d:65:70:2a:e6.home>
2026-02-13 19:34:10 -0400

    feat: add particle system file skeleton

---
+++ particle-system.js
@@ -0,0 +1,24 @@
+/**
+ * Morphing Particle System
+ * Three.js-based particle morphing with boids flocking and mouse interaction
+ */
+
+class MorphingParticleEngine {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.scene = null;
+        this.camera = null;
+        this.renderer = null;
+        this.particleSystem = null;
+        this.formationController = null;
+        this.clock = new THREE.Clock();
+    }
+
+    init() {
+        // TODO: Initialize Three.js scene
+    }
+
+    animate() {
+        // TODO: Animation loop
+    }
+}
